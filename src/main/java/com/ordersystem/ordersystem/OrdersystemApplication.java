6602cad97f98c633e1ebc72b996bef0b813328c0
LiChao <lc2510834510@gamil.com>
2019-05-09 22:20:57 +0800

    first commit

---
+++ src/main/java/com/ordersystem/ordersystem/OrdersystemApplication.java
@@ -0,0 +1,13 @@
+package com.ordersystem.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ordersystem/ordersystem/OrdersystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ordersystem.ordersystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdersystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
