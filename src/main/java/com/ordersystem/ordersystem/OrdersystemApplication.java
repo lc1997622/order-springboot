a9ed4e616b3b38ee1693992f43389f52ae1b672b
LiChao <lc2510834510@gamil.com>
2019-05-15 20:11:21 +0800

    dont konew

--- src/main/java/com/ordersystem/ordersystem/OrdersystemApplication.java
+++ src/main/java/com/ordersystem/ordersystem/OrdersystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrdersystemApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(OrdersystemApplication.class, args);
     }
 
