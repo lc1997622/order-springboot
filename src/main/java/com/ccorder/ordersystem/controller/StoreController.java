942d1f999d5c6317d2aa117906857bf1d6f2be88
htj123 <1095833846@qq.com>
2019-05-31 15:59:13 +0800

    商家信息修改传入为json

--- src/main/java/com/ccorder/ordersystem/controller/StoreController.java
+++ src/main/java/com/ccorder/ordersystem/controller/StoreController.java
@@ -66,48 +66,16 @@ public class StoreController {
     @PostMapping("updateStoreInfo")
     @ResponseBody
     public Object updateStoreInfo(
-            @ApiParam(name = "storeId", value = "商家主键id", required = true, type = "String")
-            @RequestParam("storeId")
-                    String storeId,
-            @ApiParam(name = "storeName", value = "商家店名", required = true, type = "String")
-            @RequestParam("storeName")
-                    String storeName,
-            @ApiParam(name = "storeAddress", value = "商家地址", required = true, type = "String")
-            @RequestParam("storeAddress")
-                    String storeAddress,
-            @ApiParam(name = "shipFee", value = "配送费", required = true, type = "Double")
-            @RequestParam("shipFee")
-                    Double shipFee,
-            @ApiParam(name = "shopStatus", value = "营业状态", required = true, type = "Integer")
-            @RequestParam("shopStatus")
-                    Integer shopStatus,
-            @ApiParam(name = "storeIntroduce", value = "商家介绍信息", required = true, type = "String")
-            @RequestParam("storeIntroduce")
-                    String storeIntroduce
+            @ApiParam(name = "sysUser", value = "商家主键id", required = true, type = "SysUser")
+            @RequestBody
+                    SysUser sysUser
     ) {
         try {
-            /*获取原有店铺user*/
-            SysUser storeNow = userService.selectByPrimaryKey(storeId);
-            /*获取店铺原有地址*/
-            String addressId = mapUserAddressService.selectByUserId(storeId).get(0).getAddressId();
-            Address oldAddress = addressService.selectByPrimaryKey(addressId);
-            /*修改店铺地址*/
-            oldAddress.setAddressName(storeAddress);
-            addressService.updateByPrimaryKey(oldAddress);
-            /*更新用户信息*/
-            storeNow.setRealName(storeName);
-            storeNow.setShipFee(shipFee);
-            storeNow.setStatus(shopStatus);
-            storeNow.setBusinessIntroduction(storeIntroduce);
-
-            int updateRes = userService.updateByPrimaryKeySelective(storeNow);
-            if (updateRes == 1) {
-                return new AjaxMessage().Set(MsgType.Success, "更新商家信息成功", storeNow);
-            }
+            sysUserService.updateByPrimaryKeySelective(sysUser);
+            return new AjaxMessage().Set(MsgType.Success, "更新商家信息成功");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return new AjaxMessage().Set(MsgType.Error, "更新商家信息失败", null);
     }
 }
